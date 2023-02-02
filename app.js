0a1c5b94a94d8723a3c20680b56f5a7501d5640e
Lua Specht <luaspecht@gmail.com>
2023-02-02 19:58:36 -0300

    third-commit

--- app.js
+++
@@ -1,8 +0,0 @@
-const colorPicker = document.querySelector('#color-picker');
-const text = document.querySelector('.text');
-const h1 = document.querySelector('h1');
-
-colorPicker.addEventListener('input', function() {
-  text.style.color = this.value;
-  h1.style.color = this.value;
-});
