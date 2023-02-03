6a9db5edfd32917df7a5a2a4fc93d904ec2f1d27
Lua Specht <luaspecht@gmail.com>
2023-02-03 04:02:23 -0300

    smooth-commit

--- index.js
+++ index.js
@@ -15,25 +15,24 @@ function openCity(evt, cityName) {
   
     document.getElementById("defaultOpen").click();
   
-  const container = document.querySelector('#tabela');
+  const container = document.querySelector('.container');
   let colorIndicator = document.getElementById('color-indicator');
-  let tabela = document.getElementById('tabela');
   let divHeader = document.getElementById('div-header');
+  let tabcontent
   let colorPicker = new iro.ColorPicker("#color-picker", {
     width: 80,
     color: "#0F9AB9",
     borderWidth: 0.5,
     borderColor: "#fff",
-    });
+  });
 
-    colorPicker.on("color:change", function (color) {
+  colorPicker.on("color:change", function (color) {
     divHeader.style.boxShadow = "0px 0px 2px" + color.hexString;
-    });
+  });
 
-    colorPicker.on("color:change", function (color) {
-    principal.style.boxShadow = "0px 0px 5px" + color.hexString;
-    });
+  colorPicker.on("color:change", function (color) {
+    container.style.boxShadow = "0px 0px 5px" + color.hexString;
+    
+  
 
-    colorPicker.on("color:change", function (color) {
-        tabela.style.boxShadow = "0px 0px 5px" + color.hexString;
-        });
+});
